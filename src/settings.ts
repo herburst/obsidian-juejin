ee3953e77f2e3597076fbe7076f292be3e40ee5b
herburst <920168391@qq.com>
2025-05-05 15:14:29 +0800

    优化附件文件夹设置

--- src/settings.ts
+++ src/settings.ts
@@ -22,12 +22,12 @@ export class JueJinSettingTab extends PluginSettingTab {
 
 	display(): void {
 		const {containerEl} = this;
-
+		const folders = this.app.vault.getAllFolders(true).map(f => f.path)
 		containerEl.empty();
 		containerEl.createEl("h2", {text: "掘金文章助手"});
 		new Setting(containerEl)
 			.setName("附件本地化")
-			.setDesc("获取文章时，是否将文章时包含的网络资源下载到本地路径")
+			.setDesc("获取文章时，是否将文章中包含的网络资源下载到本地文件夹")
 			.addToggle(toggle => {
 				toggle.setValue(this.plugin.settings.localAnnex)
 					.onChange(async (value) => {
@@ -36,11 +36,13 @@ export class JueJinSettingTab extends PluginSettingTab {
 					})
 			})
 		new Setting(containerEl)
-			.setName("附件存储路径")
-			.setDesc("指定下载附件时的存储路径")
-			.addText(text => {
-				text.setPlaceholder("附件存储路径")
-					.setValue(this.plugin.settings.annexPath)
+			.setName("附件存储文件夹路径")
+			.setDesc("指定下载附件时的存储文件夹路径")
+			.addDropdown(dropdown => {
+				folders.forEach(folder => {
+					dropdown.addOption(folder, folder);
+				})
+				dropdown.setValue(this.plugin.settings.annexPath)
 					.onChange(async (value) => {
 						this.plugin.settings.annexPath = value;
 						await this.plugin.saveSettings();
